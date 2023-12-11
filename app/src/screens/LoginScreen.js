667f1ec7786c67809dfa50dbf0b904e9c91c8eb4
Pranavan Sudhahar <pranavan@codelantic.com>
2023-12-11 15:38:21 +0530

    Error handling done

--- app/src/screens/LoginScreen.js
+++ app/src/screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Alert} from 'react-native'
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from '../components/util/auth';
@@ -9,9 +10,13 @@ function LoginScreen() {
 
   async function loginHandler({email, password}) {
     setIsAunthenticated(true);
+    try {
     await login(email, password); //forward this to createUser()
+    } catch(error){
+     Alert.alert('Authenticated Failed','Could not login. Please check your credentials or try again.'
+     )
+    }
     setIsAunthenticated(false);
-    
   }
 
   if (isAuthenticated) {
--- app/src/screens/SignupScreen.js
+++ app/src/screens/SignupScreen.js
@@ -1,3 +1,4 @@
+import {Alert} from 'react-native'
 import react,{useState} from 'react'
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import AuthContent from "../components/Auth/AuthContent";
@@ -6,16 +7,21 @@ import { createUser } from '../components/util/auth';
 function SignupScreen() {
   const [isAuthenticated, setIsAunthenticated] = useState(false);
 
-  async function signUpHandler({email, password}) {
+  async function signupHandler({email, password}) {
     setIsAunthenticated(true);
+    try {
     await createUser(email, password); //forward this to createUser()
+    } catch(error){
+     Alert.alert('Authentication Failed')
+     
+    }
     setIsAunthenticated(false);
   }
 
   if (isAuthenticated) {
     return <LoadingOverlay message="Creating User" />;
   }
-  return <AuthContent onAuthenticate={signUpHandler} />;
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
